0d6261f6893389601d972ac07f865aa812353598
seesunCSY <18179426506@163.com>
2017-07-23 08:53:20 +0800

    github用户授权后信息获取成功
    用户信息获取成功后，跳转至写好的静态网页中；
    静态网页的相关功能逻辑还未加入。

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -9,6 +9,6 @@ export default class extends Base {
    */
   indexAction(){
     //auto render template file index_index.html
-    return this.success(this._user);
+    return this.display();
   }
 }
